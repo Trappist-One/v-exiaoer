160a79aea0e549996502039ef75630939157ac67
trappist <3185641236@qq.com>
2020-11-26 17:50:00 +0800

    登录位置

---
+++ src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <v-layout>
+          <v-flex>
+            <v-card
+                class="float-right pa-2"
+                color="grey"
+                height="600"
+                width="400">
+              <v-card
+                  class="float-left"
+                  outlined>
+                justify-end
+              </v-card>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      message: '登录界面'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
